4fd85473c303e41af5412cdead492e9e4c8a86fe
Caleb Hoover <choover11@gmail.com>
2015-10-15 14:17:11 -0700

    changed code so that it uses requiresafe's formatters

--- tasks/requiresafe.js
+++ tasks/requiresafe.js
@@ -2,7 +2,9 @@
 
 var Chalk = require('chalk');
 var Table = require('cli-table');
-var RequireSafe = require('requiresafe');
+// var RequireSafe = require('requiresafe');
+var RequireSafe = require('../../cli');
+var format = RequireSafe.formatters.default;
 
 module.exports = function (grunt) {
 
@@ -33,41 +35,15 @@ module.exports = function (grunt) {
 
     RequireSafe.check(payload, function (err, data) {
 
-      if (err) {
-        return grunt.log.writeln(Chalk.yellow('(+) ') + err);
+      var output = format(err, data);
+      if (err || data.length) {
+        grunt.fail.warn(output);
+      } else {
+        grunt.log.write(output);
       }
 
-      var width = 80;
-      if (process.stdout.isTTY) {
-        width = process.stdout.getWindowSize()[0] - 10;
-      }
-
-      if (data.length === 0) {
-
-        return grunt.log.writeln(Chalk.green('(+)') + ' No known vulnerabilities found');
-      }
-
-      data.map(function (finding) {
-
-        var table = new Table({
-          head: ['', finding.title],
-          colWidths: [15, width - 15]
-        });
-
-        table.push(['Name', finding.module]);
-        table.push(['Version', finding.version]);
-        table.push(['Path', finding.path]);
-        table.push(['More Info', finding.advisory]);
-
-        grunt.log.write(table.toString());
-        grunt.log.write('\n');
-
-      });
-
-      grunt.fail.warn(Chalk.red('(+) ') + data.length + ' vulnerabilities found\n');
       done();
     });
-
 
   });
 };
